e0e1b515a654ccc4f709122861a1ab8456b176f0
Nick Pope <psydpope@gmail.com>
2020-03-31 16:46:56 +0000

    using question class to ask player new question

--- Game.rb
+++ Game.rb
@@ -1,6 +1,7 @@
 class Game
 
   def initialize
+
   end
 
   def start_game
@@ -11,20 +12,29 @@ class Game
     puts "-------"
     puts "When either one of the players runs out of\nlives, the game ends."
     puts "---------"
-      puts "If you're ready to start, type 'y' in the prompt"
+    puts "If you're ready to start, type 'y' in the prompt"
     ask_to_start
   end
-
+  #ask both players if they are ready to start
   def ask_to_start
-    (1..2).each do |num|
-      puts "\nPlayer #{num}: Ready to start?"
+    (1..2).each do |id|
+      puts "\nPlayer #{id}: Ready to start?"
       print "> "
       answer = gets.chomp
+      #keep asking until they enter y
       while (answer != "y")
         puts "Please type 'y' to start. Ready to start?"
         print "> "
         answer = gets.chomp
       end
     end
-    puts "\n~~~~~~~~~~ The Game starts now! ~~~~~~~~~~"
-end 
+    puts "\n~~~~~~~~~~ The Game starts... now! ~~~~~~~~~~"
+    ask_question
+  end
+
+  # Asks the current player a random question
+  def ask_question
+    question = Question.new
+    puts "\nPlayer 1: #{question.text}"
+  end 
+end
